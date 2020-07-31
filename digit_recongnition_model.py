4e2882029fc25ba80cd7b27da9385a8ed3574fbb
Mireille Gendy <mireillegendy@hotmail.ca>
2020-07-30 21:30:22 -0400

    Added machine learning compoenent for digit recognition in file digit_recognition_model

---
+++ digit_recongnition_model.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test= tf.keras.utils.normalize(x_test, axis=1)
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metric=['accuracy'])
+model.fit(x_train, y_train, epochs=3)
+val_loss = model.evaluate(x_test, y_test)
+print(val_loss)
+model.save('Digit_reader')
+final_model = tf.keras.models.load_model('Digit_reader')
